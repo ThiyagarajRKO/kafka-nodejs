2c578e6009916dfb93aa83ea02fcbbbd294a9eca
Thiyagaraj <ThiyagarajRKO@gmail.com>
2024-10-15 12:00:16 +0530

    added task blueprint

--- src/routes/campaign/handlers/create_campaign.js
+++ src/routes/campaign/handlers/create_campaign.js
@@ -1,6 +1,6 @@
 import { GetResponse } from "../../../../utils/node-fetch";
 
-const requiredCFIds = [
+const requiredCFNames = [
   "Campaign Name*",
   "Campaign Objective*",
   "Campaign Start Date*",
@@ -17,6 +17,23 @@ const requiredCFIds = [
   "CCUID*",
 ];
 
+const requiredCFIds = {
+  TEST: [
+    "IEABCEFLJUAEF4B7",
+    "IEABCEFLJUAEF4AJ",
+    "IEABCEFLJUAEGPM6",
+    "IEABCEFLJUAEF4AO",
+    "IEABCEFLJUAEGZFP",
+    "IEABCEFLJUAEF4BI",
+    "IEABCEFLJUAEF4BJ",
+    "IEABCEFLJUAEGGC3",
+    "IEABCEFLJUAEF4AI",
+    "IEABCEFLJUAGCS2I",
+    "IEABCEFLJUAGCS2J",
+    "IEABCEFLJUAEF4AK",
+  ],
+};
+
 export const CreateCampaign = (wrikeToken, params, fastify) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -25,34 +42,105 @@ export const CreateCampaign = (wrikeToken, params, fastify) => {
       }
 
       // Variable Declaration
-      let customFieldIds = [];
+      const {
+        spaceName,
+        folderId,
+        campaignBlueprintId,
+        listOfChannelBlueprintId,
+        wrikeCampaign,
+      } = params;
 
-      // Get Custom Field Ids
-      const customFieldsData = await GetResponse(
-        `${process.env.WRIKE_ENDPOINT}/customfields`,
-        "GET",
+      // // Get Custom Field Ids
+      // const customFieldsData = await GetResponse(
+      //   `${process.env.WRIKE_ENDPOINT}/customfields`,
+      //   "GET",
+      //   {
+      //     "content-type": "application/json",
+      //     Authorization: `Bearer ${wrikeToken}`,
+      //   }
+      // );
+
+      // if (!customFieldsData || customFieldsData?.data?.length == 0) {
+      //   return reject({ message: "Failed to fetch custom field ids!" });
+      // }
+
+      // // Extracting valid CF ids
+      // await Promise.all(
+      //   customFieldsData?.data?.map((data) => {
+      //     if (requiredCFIds.includes(data?.title)) {
+      //       customFieldIds.push(data?.id);
+      //     }
+      //   })
+      // );
+
+      // Clone folder blueprint
+      const folderBlueprintData = await GetResponse(
+        `${process.env.WRIKE_ENDPOINT}/folder_blueprints/${campaignBlueprintId}/launch_async`,
+        "POST",
         {
           "content-type": "application/json",
           Authorization: `Bearer ${wrikeToken}`,
+        },
+        {
+          parent: folderId,
+          title: wrikeCampaign?.campaignName,
         }
       );
 
-      console.log(customFieldsData?.data);
-
-      if (!customFieldsData || customFieldsData?.data?.length == 0) {
-        return reject({ message: "Failed to fetch custom field ids!" });
+      if (folderBlueprintData?.errorDescription) {
+        return reject(folderBlueprintData);
       }
 
-      await Promise.all(
-        customFieldsData?.data?.map((data) => {
-          if (requiredCFIds.includes(data?.title)) {
-            customFieldIds.push(data?.id);
+      // listOfChannelBlueprintId.forEach(async (taskBlueprintId) => {
+      for (let i = 0; i < listOfChannelBlueprintId.length; i++) {
+        const taskBlueprintId = listOfChannelBlueprintId[i];
+
+        if (!taskBlueprintId) return;
+
+        const taskBlueprintData = await GetResponse(
+          `${process.env.WRIKE_ENDPOINT}/task_blueprints/${taskBlueprintId}/launch_async`,
+          "POST",
+          {
+            "content-type": "application/json",
+            Authorization: `Bearer ${wrikeToken}`,
+          },
+          {
+            parentId: folderId,
+            title: wrikeCampaign?.campaignName,
           }
-        })
-      );
+        );
+
+        if (taskBlueprintData?.errorDescription) {
+          return reject(taskBlueprintData);
+        }
+      }
 
       resolve({
-        data: { customFieldIds },
+        message: "Campaign has been created successfully",
+        data: {
+          campaignName: wrikeCampaign?.campaignName,
+          campaignDescription: wrikeCampaign?.campaignDescription,
+          campaignObjective: wrikeCampaign?.campaignObjective,
+          campaignStartDate: wrikeCampaign?.campaignStartDate,
+          campaignEndDate: wrikeCampaign?.campaignEndDate,
+          biddableNonbiddable: wrikeCampaign?.biddableNonbiddable,
+          currency: wrikeCampaign?.currency,
+          campaignBudget: wrikeCampaign?.campaignBudget,
+          requestorMarket: wrikeCampaign?.requestorMarket,
+          agency: wrikeCampaign?.agency,
+          client: wrikeCampaign?.client,
+          debtor: wrikeCampaign?.debtor,
+          brand: wrikeCampaign?.brand,
+          folderPath: wrikeCampaign?.folderPath,
+          cssid: wrikeCampaign?.cssid,
+          ccuid: wrikeCampaign?.ccuid,
+          customFields: {},
+          listOfTaskIds: listOfChannelBlueprintId,
+          listOfChannelIds: campaignBlueprintId,
+          wrikev2id: wrikeCampaign?.wrikev2id,
+          wrikev3id: wrikeCampaign?.wrikev3id,
+          wrikePermalink: wrikeCampaign?.wrikePermalink,
+        },
       });
     } catch (err) {
       console.log(err?.message || err);
