40d18a45237415d0ab398bf96029e2a37df0b38d
Thiyagaraj <ThiyagarajRKO@gmail.com>
2024-12-11 10:05:21 +0530

    added live customfield ids

--- src/routes/copyToChild/utils/customFieldsIds.js
+++ src/routes/copyToChild/utils/customFieldsIds.js
@@ -1,10 +1,18 @@
 module.exports = {
-  "test": {
+  test: {
     "CopyToChild*": "IEABCEFLJUAEF4SP",
     "Offshore Global Hub*": "IEABCEFLJUAEF4AL",
     "Agency*": "IEABCEFLJUAEF4AI",
     "Region*": "IEABCEFLJUAEGZFK",
     "Client*": "IEABCEFLJUAEF4AK",
-    "Practice [GMS]": "IEABCEFLJUAEGZFO"
-  }
-}
+    "Practice [GMS]": "IEABCEFLJUAEGZFO",
+  },
+  live: {
+    "CopyToChild*": "IEAC7PRTJUADKV3A",
+    "Offshore Global Hub*": "IEAC7PRTJUAB43DM",
+    "Agency*": "IEAC7PRTJUAB43DB",
+    "Region*": "IEAC7PRTJUAB432Y",
+    "Client*": "IEAC7PRTJUAGPHMQ",
+    "Practice [GMS]": "IEAC7PRTJUAB455U",
+  },
+};
