e5246fbe3cfb93859d8cc7535225fbbb4fbfa5c9
ThiyagarajRKO <ThiyagarajRKO@gmail.com>
2025-01-24 10:49:40 +0530

    added error log file

--- src/utils/wingston.js
+++ src/utils/wingston.js
@@ -1,5 +1,9 @@
 import winston from "winston";
 import path from "path";
+import moment from "moment";
+
+// Generate the log file names with the current date
+const currentDate = moment().format("YYYYMMDD"); // Format date as YYYYMMDD
 
 // Create a custom log format to include topic and data
 const customFormat = winston.format.printf(
@@ -24,9 +28,26 @@ const logger = winston.createLogger({
   transports: [
     // Write logs to a file
     new winston.transports.File({
-      filename: path.join(__dirname, "/../../logs", "app.log"),
+      filename: path.join(
+        __dirname,
+        "/../../logs",
+        `app-error-${currentDate}.log`
+      ),
       maxsize: 5242880, // 5MB max size
       maxFiles: 5, // Keep 5 rotated files
+      level: "error",
+    }),
+
+    // Write info logs to a file with the current date
+    new winston.transports.File({
+      filename: path.join(
+        __dirname,
+        "/../../logs",
+        `app-info-${currentDate}.log`
+      ),
+      maxsize: 5242880, // 5MB max size
+      maxFiles: 5, // Keep 5 rotated files
+      level: "info",
     }),
     // Optionally log to the console for debugging
     // new winston.transports.Console(),
